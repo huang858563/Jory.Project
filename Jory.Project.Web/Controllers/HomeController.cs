a23469b8d8147b6ac5339178be925181b79f4df2
huang858563 <huang858563@163.com>
2018-10-21 23:36:27 +0800

    one

--- Jory.Project.Web/Controllers/HomeController.cs
+++ Jory.Project.Web/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Jory.Project.Web.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Yx.";
 
             return View();
         }
