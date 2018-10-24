24d90738506bb054154b5f70984a1f2340775de6
huang858563 <huang858563@163.com>
2018-10-24 20:53:48 +0800

    asdf

--- Jory.Project.Web/Controllers/HomeController.cs
+++ Jory.Project.Web/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Jory.Project.Web.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "aabbbb.";
+            ViewData["Message"] = "a";
 
             return View();
         }
